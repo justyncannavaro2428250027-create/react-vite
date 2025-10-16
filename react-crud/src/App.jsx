8f3f3fbe5d584131f32f39d9aca1764389bfa4c2
JustynCannavaro09 <cranelzeus1@gmail.com>
2025-10-16 17:26:48 +0700

    Update App.jsx

--- react-crud/src/App.jsx
+++ react-crud/src/App.jsx
@@ -31,10 +31,6 @@ function App() {
           <NavLink className="nav-link" to="prodi">Prodi</NavLink>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
